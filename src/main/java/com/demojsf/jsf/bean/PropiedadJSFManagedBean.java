f3d3daad10e97efcaa03e71cff94596be49d7583
Carlos <Carlos@192.168.1.7>
2018-10-04 00:49:54 -0500

    Se agrego exportacion a los demas modulos, menos facturacion, comision y recuado por lo que aun no estan listos...

--- src/main/java/com/demojsf/jsf/bean/PropiedadJSFManagedBean.java
+++ src/main/java/com/demojsf/jsf/bean/PropiedadJSFManagedBean.java
@@ -10,6 +10,10 @@ import java.util.List;
 import javax.annotation.PostConstruct;
 import javax.inject.Named;
 import javax.faces.view.ViewScoped;
+import org.primefaces.model.chart.Axis;
+import org.primefaces.model.chart.AxisType;
+import org.primefaces.model.chart.BarChartModel;
+import org.primefaces.model.chart.ChartSeries;
 
 @Named(value = "propiedadBean")
 @ViewScoped
@@ -21,6 +25,8 @@ public class PropiedadJSFManagedBean implements Serializable {
     private DaoPropiedades dao = new DaoPropiedadImpl();
     private boolean modoInsert = false;
     private boolean modoEdit = true;
+    private ChartSeries PropiedData;
+    private BarChartModel barModel;
 
     public boolean isModoInsert() {
         return modoInsert;
@@ -46,6 +52,7 @@ public class PropiedadJSFManagedBean implements Serializable {
     public void iniciar() {
         lista = dao.getPropiedad();
         propiedad.setIdpropiedad(lista.size() + 1);
+        createBarModel();
     }
 
     public List<Propiedad> getLista() {
@@ -94,5 +101,42 @@ public class PropiedadJSFManagedBean implements Serializable {
     public void changeMode() {
         modoEdit = false;
         modoInsert = true;
+    }
+    
+    public BarChartModel getBarModel(){
+        return barModel;
+    }
+    
+    private BarChartModel initBarModel(){
+        BarChartModel model = new BarChartModel();
+        
+        PropiedData = new ChartSeries();
+        PropiedData.setLabel("valor");
+        for (Propiedad cData: lista){
+            PropiedData.set(cData.getTipo(), cData.getValor());
+        }
+        
+        model.addSeries(PropiedData);
+        
+        return model;
+               
+    }
+    
+    private void createBarModel(){
+        barModel = initBarModel();
+        
+        barModel.setTitle("Propiedad mas costosa");
+        barModel.setLegendPosition("ne");
+        
+        Axis xAxis = barModel.getAxis(AxisType.X);
+        xAxis.setLabel("Tipo");
+        
+        Axis yAxis = barModel.getAxis(AxisType.Y);
+        yAxis.setLabel("Valor");
+        yAxis.setMin(0);
+        yAxis.setMax(75);
+        
+        
+        
     }
 }
